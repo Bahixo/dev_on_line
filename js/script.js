7da1e588652daa898a5d43d8e07ebc10efde81e9
Bahixo <bahixo@hotmail.fr>
2020-03-30 00:09:16 +0200

    ajout de commentaire pour Joel + fix menu 768+ via js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,36 @@
+//-----------------------------------------Fonction calculant la hauteur pour le slide une fois la page chargée
 function ratioSlide(){
     let hSlide=document.querySelector("#slide div").offsetWidth*0.7;
-    console.log(hSlide);
     String(hSlide);
     hSlide+="px";
-    console.log(hSlide);
     $("#slide div").css("height", hSlide);
 }
 
-
+//------------------------------------------Fonction du slide permettant le changement d'image toute les 10 secondes
 let t=0;
 function sliding(){
-    console.log("Coucou "+ t);
     t++;
     if(t>=6){
         t=0;
     }
-    //$("#slide span").removeClass("activ");
-    let element=document.getElementById(t);
-    //$ (element).addClass("activ");
+    //$("#slide span").removeClass("activ");//----|
+    let element=document.getElementById(t);//     |-----anticipation d'une class "activ" en cas d'ajout de modification sur l'image active
+    //$ (element).addClass("activ");//------------|
     $("#slide div").css("background-image", $(element).attr("data-pic"));
 }
-
-let menuTab= document.getElementsByClassName("menuB");
-$ ("menuB").css("margin-top", "70px");
+//------------------------------------------Fonction du menu burger présent en version mobile
 function menu(){
-    if($(".menuB").css("visibility")==="hidden"){
-        $(".menuB").css("visibility","visible");
-        $("#menuPic").css("background-color", "#7aa6cf");
-        $("#menuPic").css("border", "1px solid #c24595");
-        $("#menuPic").css("box-shadow", "5px 5px 5px grey");
-    }else{
-        $(".menuB").css("visibility","hidden");
-        $("#menuPic").css("background-color","transparent");
-        $("#menuPic").css("border", "none");
-        $("#menuPic").css("box-shadow", "none");
+    if(document.querySelector("body").offsetWidth<768){
+        if($(".menuB").css("visibility")==="hidden"){
+            $(".menuB").css("visibility","visible");
+            $("#menuPic").css("background-color", "#7aa6cf");
+            $("#menuPic").css("border", "1px solid #c24595");
+            $("#menuPic").css("box-shadow", "5px 5px 5px grey");
+        }else{
+            $(".menuB").css("visibility","hidden");
+            $("#menuPic").css("background-color","transparent");
+            $("#menuPic").css("border", "none");
+            $("#menuPic").css("box-shadow", "none");
+        }
     }
 }
